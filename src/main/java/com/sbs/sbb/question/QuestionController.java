151686cd2c7f0b694bab64804584d7f3d001effb
parkhyeo <hyeonjae1023111@gmail.com>
2023-09-01 02:01:12 +0900

    ã오류해결(GetMapping의 ãcreate 메서드가 필요함) 답변등록 폼 구현

--- src/main/java/com/sbs/sbb/question/QuestionController.java
+++ src/main/java/com/sbs/sbb/question/QuestionController.java
@@ -1,5 +1,6 @@
 package com.sbs.sbb.question;
 
+import com.sbs.sbb.answer.AnswerForm;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -23,10 +24,14 @@ public class QuestionController {
         return "question_list";
     }
     @GetMapping(value = "/detail/{id}")
-    public String detail(Model model,@PathVariable("id") Integer id) {
+    public String detail(Model model,@PathVariable("id") Integer id, AnswerForm answerForm) {
         Question question = this.questionService.getQuestion(id);
         model.addAttribute("question",question);
         return "question_detail";
+    }
+    @GetMapping("/create")
+    public String questionCreate(QuestionForm questionForm) {
+        return "question_form";
     }
     @PostMapping("/create")
     public String questionCreate(@Valid QuestionForm questionForm, BindingResult bindingResult) {
